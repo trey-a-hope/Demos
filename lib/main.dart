0de9b9f459e69b2dc25614f1a911f0536e6085ce
Trey Hope <trey.a.hope@gmail.com>
2021-09-07 13:26:01 -0600

    Ran through demo.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,51 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:app_tracking_transparency/app_tracking_transparency.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(MyApp());
+  final TrackingStatus status =
+      await AppTrackingTransparency.requestTrackingAuthorization();
+
+  runApp(MyApp(status: status));
 }
 
 class MyApp extends StatelessWidget {
+  final TrackingStatus status;
+
+  MyApp({required this.status});
+
   @override
   Widget build(BuildContext context) {
+    String statusText;
+
+    switch (status) {
+      case TrackingStatus.authorized:
+        statusText = 'Tracking Status Authorized';
+        break;
+      case TrackingStatus.denied:
+        statusText = 'Tracking Status Denied';
+        break;
+      case TrackingStatus.notDetermined:
+        statusText = 'Tracking Status Not Determined';
+        break;
+      case TrackingStatus.notSupported:
+        statusText = 'Tracking Status Not Supported';
+        break;
+      case TrackingStatus.restricted:
+        statusText = 'Tracking Status Restricted';
+        break;
+      default:
+        statusText = 'You should not see this...';
+        break;
+    }
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('App Tracking Transparancy')),
         body: Center(
-          child: Text('Body'),
+          child: Text(statusText),
         ),
       ),
     );
