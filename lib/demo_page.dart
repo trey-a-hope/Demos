d1b31431d4a971ce0bc5f98e3bf03cbc24efc77a
Trey Hope <trey.a.hope@gmail.com>
2021-11-05 18:22:37 -0400

    Added demo page.

---
+++ lib/demo_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DemoPage extends StatefulWidget {
+  @override
+  _DemoPageState createState() => _DemoPageState();
+}
+
+class _DemoPageState extends State<DemoPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Demo'),
+      ),
+      body: Center(
+        child: Text('Demo Page'),
+      ),
+    );
+  }
+}
