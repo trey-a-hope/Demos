e19e0dfd36e2a2e84d35d26b6f0438636db53964
Trey Hope <trey.a.hope@gmail.com>
2022-06-24 17:38:05 -0400

    * Added basic firebase files.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:patreon/demo_page.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
 
   runApp(MyApp());
 }
