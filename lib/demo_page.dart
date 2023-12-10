80e4439ed33f36fceefc8ed2ba9bac9ddeca7903
Trey Hope <99027548+trey-a-hope@users.noreply.github.com>
2023-12-10 13:43:31 -0600

    Ground Zero
    Ground Zero

--- lib/demo_page.dart
+++ lib/demo_page.dart
@@ -1,13 +1,7 @@
-import 'package:demos/local_type.dart';
-import 'package:demos/my_custom_messages.dart';
 import 'package:flutter/material.dart';
-import 'package:timeago/timeago.dart' as timeago;
 
 class DemoPage extends StatefulWidget {
-  const DemoPage({
-    Key? key,
-    required this.title,
-  }) : super(key: key);
+  const DemoPage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -16,34 +10,10 @@ class DemoPage extends StatefulWidget {
 }
 
 class DemoPageState extends State<DemoPage> {
-  final fifteenMinsAgo = DateTime.now().subtract(const Duration(minutes: 15));
-  final fifteenMinsBefore = DateTime.now().add(const Duration(minutes: 15));
-  final oneMinAgo = DateTime.now().subtract(const Duration(minutes: 1));
-
   @override
   void initState() {
     super.initState();
-
-    _initTimeago();
   }
-
-  // Timeago library ONLY includes en and es messages loaded by default.
-  void _initTimeago() {
-    // Add the French locale.
-    timeago.setLocaleMessages(LocaleType.fr.locale, timeago.FrMessages());
-
-    // Add my custom message locale, (overrides the 'en' locale).
-    timeago.setLocaleMessages(LocaleType.en.locale, MyCustomMessages());
-  }
-
-  ListTile _builtTimeagoListTile(DateTime date, LocaleType localeType) =>
-      ListTile(
-        title: Text(localeType.name),
-        subtitle: Text(
-          timeago.format(date,
-              locale: localeType.locale, clock: oneMinAgo, allowFromNow: true),
-        ),
-      );
 
   @override
   Widget build(BuildContext context) {
@@ -51,14 +21,8 @@ class DemoPageState extends State<DemoPage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column(
-        children: [
-          for (var i = 0; i < LocaleType.values.length; i++) ...[
-            _builtTimeagoListTile(fifteenMinsAgo, LocaleType.values[i]),
-            _builtTimeagoListTile(fifteenMinsBefore, LocaleType.values[i]),
-            const Divider(),
-          ]
-        ],
+      body: Center(
+        child: Text('${widget.title} Page'),
       ),
     );
   }
