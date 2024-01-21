1b94e33a8b58651b4576c45f7c5f04458eddae47
Trey Hope <99027548+trey-a-hope@users.noreply.github.com>
2024-01-20 23:22:53 -0600

    AR Kit Shapes
    * Added sphere, face, and capsule nodes to the ARKit screen.

--- lib/demo_page.dart
+++ lib/demo_page.dart
@@ -1,4 +1,6 @@
+import 'package:arkit_plugin/arkit_plugin.dart';
 import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart';
 
 class DemoPage extends StatefulWidget {
   const DemoPage({super.key});
@@ -15,9 +17,37 @@ class _DemoPageState extends State<DemoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Arkit Plugin'),
+      body: ARKitSceneView(
+        onARKitViewCreated: (c) {
+          c.add(WhiteSphereNode());
+          c.add(FaceNode());
+          c.add(CapsuleNode());
+        },
       ),
     );
   }
+}
+
+class WhiteSphereNode extends ARKitNode {
+  WhiteSphereNode({double radius = 0.1})
+      : super(
+          geometry: ARKitSphere(radius: radius),
+          position: Vector3(0, 0, -0.5),
+        );
+}
+
+class FaceNode extends ARKitNode {
+  FaceNode({double radius = 0.1})
+      : super(
+          geometry: ARKitFace(),
+          position: Vector3(1, 0, -0.5),
+        );
+}
+
+class CapsuleNode extends ARKitNode {
+  CapsuleNode({double radius = 0.1})
+      : super(
+          geometry: ARKitCapsule(capRadius: radius),
+          position: Vector3(-1, 1, -0.5),
+        );
 }
